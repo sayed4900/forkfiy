591236bcac1d976c4034f24df67e6ce35771c527
unknown <sayedabdo4900@gmail.com>
2022-08-10 21:52:11 +0200

    first

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,9 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
-import { render } from 'sass';
+import paginationView from './views/paginationView.js';
+
+// import { render } from 'sass';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
@@ -41,13 +43,27 @@ const controlSearchResults = async function () {
 
     //3-Render results
 
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    //4-Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
+
+const controlPagination = function (goToPage) {
+  //1-Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //2-Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHanlderSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,14 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -49,4 +52,11 @@ export const loadSearchResults = async function (query) {
     console.error(`${err} ⬇️⬇️⬇️`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
 };
