49803fdab42aef403fb9e21f444ac7a74c25d3f7
unknown <sayedabdo4900@gmail.com>
2022-08-10 15:31:10 +0200

    first

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 import { render } from 'sass';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -8,7 +9,9 @@ import { async } from 'regenerator-runtime';
 
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
-
+if (module.hot) {
+  module.hot.accept();
+}
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -28,6 +31,8 @@ const controlRecipes = async function () {
 };
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     //1-Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -35,7 +40,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3-Render results
-    console.log(model.state.search.results);
+
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
